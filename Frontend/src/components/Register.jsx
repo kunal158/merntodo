a81b0a03c2fa570b916c70634e7f2a2aa59d0e40
KUNAL GOSWAMI <76223437+kunal158@users.noreply.github.com>
2024-04-11 15:30:54 +0530

    Update Register.jsx

--- Frontend/src/components/Register.jsx
+++ Frontend/src/components/Register.jsx
@@ -21,7 +21,6 @@ function Register() {
     e.preventDefault();
     try {
       const response = await fetch('https://todoing-wheat.vercel.app/api/signup', {
-        mode: 'no-cors',
         method: "POST",
         headers: {
           "Content-Type": "application/json",
