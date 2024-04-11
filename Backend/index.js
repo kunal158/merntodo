66adc62d6e943253294c5569f6e67127dd878438
KUNAL GOSWAMI <kgreat1508@gmail.com>
2024-04-11 14:21:15 +0530

    Deploying

--- Backend/index.js
+++ Backend/index.js
@@ -18,7 +18,13 @@ connectDB();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors(
+  {
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+  }
+));
 
 // Routes
 app.use("/api", signup);
